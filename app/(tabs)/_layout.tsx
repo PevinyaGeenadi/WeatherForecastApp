cb4c339e14f99ad083a3f6a727ce3ca808ed305b
PevinyaGeenadi <haputhanthri200@gmail.com>
2024-07-17 11:16:17 +0530

    Adding a icon to Home screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,14 @@
+import { Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 
 export default function TabLayout() {
   return (
     <Tabs>
-     <Tabs.Screen name="index" options={{ title: "Home" , headerShown: false }} />  
+    <Tabs.Screen name="index" options={{ title: "Home" , headerShown: false, tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" color={color} size={size} />
+          ),
+        }}
+      /> 
      <Tabs.Screen name="setting" options={{ title: "Settings" , headerShown: false }} />
     </Tabs>
   );
