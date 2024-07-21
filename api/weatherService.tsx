fbb5f2a622fbe4d54244530fb1461bc9d5011b74
PevinyaGeenadi <haputhanthri200@gmail.com>
2024-07-21 13:27:28 +0530

    Updating package files and adding new weather images and weather API constants

---
+++ api/weatherService.tsx
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+
+const BASE_URL = 'https://api.weatherapi.com/v1/forecast.json?key=14c7225f7fdf45d68e564028242107&q=London&days=1&aqi=no&alerts=no';
+
---
+++ constants/api.ts
@@ -0,0 +1,21 @@
+export const apiKey = '14c7225f7fdf45d68e564028242107';
+
+export const weatherImages = {
+    'Clear': require('../assets/images/sun1.png'),
+    'Cloudy': require('../assets/images/cloud.png'),
+    'Heavy rain': require('../assets/images/heavyrain.png'),
+    'Heavy rain at times': require('../assets/images/heavyrain.png'),
+    'Light rain': require('../assets/images/moderaterain.png'),
+    'Moderate or heavy freezing rain': require('../assets/images/heavyrain.png'),
+    'Moderate or heavy rain shower': require('../assets/images/heavyrain.png'),
+    'Moderate or heavy rain with thunder': require('../assets/images/heavyrain.png'),
+    'Moderate rain': require('../assets/images/moderaterain.png'),
+    'Moderate rain at times': require('../assets/images/moderaterain.png'),
+    'Overcast': require('../assets/images/cloud.png'),
+    'Partly cloudy': require('../assets/images/partlycloudy.png'),
+    'Patchy rain possible': require('../assets/images/moderaterain.png'),
+    'Sunny': require('../assets/images/sun1.png'),
+    'Other': require('../assets/images/moderaterain.png'),
+  };
+  
+  
