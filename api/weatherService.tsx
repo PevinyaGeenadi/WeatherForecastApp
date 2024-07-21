84c557a4a4cb1bd4fa91d4ba9207fcf0ea0f1195
PevinyaGeenadi <haputhanthri200@gmail.com>
2024-07-21 16:21:33 +0530

    Adding weather forecast and location fetching functionality

--- api/weatherService.tsx
+++ api/weatherService.tsx
@@ -1,5 +1,28 @@
 import axios from "axios";
+import { apiKey } from "../constants/api";
 
+const forecastEndpoint = (params: { cityName: string, days: number }) => `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${params.cityName}&days=${params.days}&aqi=no&alerts=no`;
 
-const BASE_URL = 'https://api.weatherapi.com/v1/forecast.json?key=14c7225f7fdf45d68e564028242107&q=London&days=1&aqi=no&alerts=no';
+const locationsEndpoint = (params: { cityName: string }) => `https://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${params.cityName}`;
 
+const getWeather = async (endpoint: string) => {
+    const options = {
+        method: 'GET',
+        url: endpoint,
+    };
+    try {
+        const response = await axios.request(options);
+        return response.data;
+    } catch (error) {
+        console.log('Error fetching weather data:', error);
+        return null;
+    }
+  };
+
+export const fetchWeatherForecast = (params: { cityName: string, days: number }) => {
+    return getWeather(forecastEndpoint(params));
+}
+
+export const fetchLocations = (params: { cityName: string }) => {
+    return getWeather(locationsEndpoint(params));
+}
